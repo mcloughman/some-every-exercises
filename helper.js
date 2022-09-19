fcae494aaa68a01bb8746ecd2131c9e1ba1008fe
Michael Loughman <mcloughman@gmail.com>
2022-09-19 14:35:42 -0700

    all tests pass

---
+++ helper.js
@@ -0,0 +1,11 @@
+// var arr = [
+//     {title: "Instructor", first: 'Elie', last:"Schoppik"}, 
+//     {title: "Instructor", first: 'Tim', last:"Garcia", isCatOwner: true}, 
+//     {title: "Instructor", first: 'Matt', last:"Lane"}, 
+//     {title: "Instructor", first: 'Colt', last:"Steele", isCatOwner: true}
+// ]
+
+// console.log(hasCertainKey(arr, "first"))
+
+
+console.log(hasNoDuplicates([1, 2, 3]));
